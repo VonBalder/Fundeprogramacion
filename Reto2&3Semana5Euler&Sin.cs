e0969d78c1a20996e7fe2eaf83e9886cd9c6acf8
VonBalder <69406629+VonBalder@users.noreply.github.com>
2021-02-23 07:24:11 -0500

    Update Reto2&3Semana5Euler&Sin.cs

--- Reto2&3Semana5Euler&Sin.cs
+++ Reto2&3Semana5Euler&Sin.cs
@@ -1,50 +1,61 @@
 using System;
 
-public class Program {
-  public static void Main() {
+namespace EulerSin
+{
+    class Program
+    {
+        public static void Main()
+        {
 
-        //profe mil disculpas no hay video, estoy muy agripado.
+            //profe mil disculpas no hay video, estoy muy agripado.
 
-    // Euler
-    Console.WriteLine(Euler(3, 100)); // 20.085
+            // Euler
+            Console.WriteLine(Euler(3, 100)); // 20.085
 
-    // Sin
-    Console.WriteLine(Sin(0.5, 100)); // 0.48
-    Console.WriteLine(Sin(1.25, 100)); // 0.95
-  }
+            // Sin
+            Console.WriteLine(Sin(0.5, 100)); // 0.48
+            Console.WriteLine(Sin(1.25, 100)); // 0.95
+        }
 
-  public static double Factorial(double n) {
-    double r = 1;
-    for (double i = n; i >= 1; --i) {
-      r *= i;
+        public static double Factorial(double n)
+        {
+            double r = 1;
+            for (double i = n; i >= 1; --i)
+            {
+                r *= i;
+            }
+            return r;
+        }
+
+        public static double Euler(double x, double n)
+        {
+            double r = 0;
+            for (int i = 0; i < n; ++i)
+            {
+                double first = Math.Pow(x, i);
+                double f = Factorial(i);
+                r += (first / f);
+            }
+            return r;
+        }
+
+        public static double Sin(double x, double n)
+        {
+            double r = 0;
+
+            for (int i = 0; i < n; ++i)
+            {
+
+                double first = Math.Pow(-1, i);
+                double sum = (2 * i) + 1;
+                double second = Factorial(sum);
+                double third = Math.Pow(x, sum);
+
+                r += ((first / second) * third);
+            }
+
+            return Math.Round(r, 2);
+
+        }
     }
-    return r;
-  }
-
-  public static double Euler(double x, double n) {
-    double r = 0;
-    for (int i = 0; i < n; ++i) {
-      double first = Math.Pow(x, i);
-      double f = Factorial(i);
-      r += (first / f);
-    }
-    return r;
-  }
-
-  public static double Sin(double x, double n) {
-    double r = 0;
-
-    for (int i = 0; i < n; ++i) {
-
-      double first = Math.Pow(-1, i);
-      double sum = (2 * i) + 1;
-      double second = Factorial(sum);
-      double third = Math.Pow(x, sum);
-
-      r += ((first / second) * third);
-    }
-
-    return Math.Round(r, 2);
-
-  }
 }
